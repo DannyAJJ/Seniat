858b509f3070ce44fda77002ca7ef8103ade3bb4
EmpGabriel <116410476+EmpGabriel@users.noreply.github.com>
2024-06-17 10:41:40 +0200

    gabriel 17-06
    estilo de usuario

--- usuarios/agregar_usuario.php
+++ usuarios/agregar_usuario.php
@@ -69,5 +69,12 @@
     })
 </script>
 
+    <main>
+        <video muted autoplay loop>
+            <source src="./video/videof.mp4" type="video/mp4">
+        </video>
+        <div class="capa"></div>
+    </main>
+
 </body>
 </html>
