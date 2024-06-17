b9f82701f222a7ed292f214b648abc16181cc2ac
DannyAJJ <dannyalejandro.jj@gmail.com>
2024-06-17 09:33:23 -0400

    Create PostTabaco.php

---
+++ formulario/PostTabaco.php
@@ -0,0 +1,13 @@
+<?php
+// Parámetros de conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "expendiobd";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+$sql="SELECT * FROM `autorizacion_licores`";
+
+?>
